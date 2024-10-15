d5e3b1c2ee64ecfdd09125b8dddfb09762ff364c
victorlga <victorluisgassis@gmail.com>
2024-10-15 11:29:28 -0300

    implement load_data in document

--- src/document.py
+++ src/document.py
@@ -1,17 +1,28 @@
+import os
+
+from langchain_community.document_loaders import PyMuPDFLoader
+
+from page import Page
+
 class Document:
 
-    def __init__(self, data: list, key: str):
-        self.data = data
+    def __init__(self, file_name: str):
+        self.file_name = file_name
         self.new_data = ""
-        self.key = key
         self.start = 0
-        self.end = len(data)
+        self.end = -1
     
-    def set_start(self, start: int):
+    def set_start(self, start: int) -> None:
         self.start = start
 
-    def set_end(self, end: int):
+    def set_end(self, end: int) -> None:
         self.end = end
+
+    def load_data(self) -> None:
+        file_path = os.path.join(os.path.dirname(__file__), '../raw', f'{self.file_name}.pdf')
+        loader = PyMuPDFLoader(file_path)
+        self.data = loader.load()
+        self.end = len(self.data)
 
     def clean(self) -> str:
         main_data = self.data[self.start:self.end]
